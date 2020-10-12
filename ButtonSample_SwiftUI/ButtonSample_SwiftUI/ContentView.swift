e6a54f242fe3f5f70ee66bf80e51ab56bfcaba98
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-12 21:07:54 +0900

    同じボタンを2つ縦並びに表示

--- ButtonSample_SwiftUI/ButtonSample_SwiftUI/ContentView.swift
+++ ButtonSample_SwiftUI/ButtonSample_SwiftUI/ContentView.swift
@@ -9,14 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var buttonTitle = "Button Sample"
+    @State var topButtonTitle = "Button Sample"
+    @State var bottomButtonTitle = "Button Sample"
     
     var body: some View {
         VStack {
             Button(action: {
-                self.buttonTitle = "Button Tapped!"
+                self.topButtonTitle = "Button Tapped!"
             }) {
-                Text("\(buttonTitle)")
+                Text("\(topButtonTitle)")
+                    .frame(width: 200, height: 70, alignment: .center)
+                    .foregroundColor(Color.white)
+                    .background(Color.blue)
+                    .cornerRadius(10, antialiased: true)
+            }
+            .padding()
+            
+            Button(action: {
+                self.bottomButtonTitle = "Button Tapped!"
+            }) {
+                Text("\(bottomButtonTitle)")
                     .frame(width: 200, height: 70, alignment: .center)
                     .foregroundColor(Color.white)
                     .background(Color.blue)
