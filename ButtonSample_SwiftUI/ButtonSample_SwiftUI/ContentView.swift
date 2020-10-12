43bcc30cc8e1b39924cb49d65d07f999ff651555
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2020-10-12 15:33:19 +0900

    初回コミット

---
+++ ButtonSample_SwiftUI/ButtonSample_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonSample_SwiftUI
+//
+//  Created by home on 2020/10/12.
+//  Copyright © 2020 Swift-beginners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
